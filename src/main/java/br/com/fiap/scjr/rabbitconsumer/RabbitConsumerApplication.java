3977ff1a5fa7f512c11b533d6cc3f58d2033c197
jhowlacerda <jonatha.lacerda@outlook.com>
2022-11-26 17:18:02 -0300

    Implementação inicial do Drone Consumer

---
+++ src/main/java/br/com/fiap/scjr/rabbitconsumer/RabbitConsumerApplication.java
@@ -0,0 +1,17 @@
+package br.com.fiap.scjr.droneconsumer;
+
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableRabbit
+@EnableScheduling
+public class RabbitConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/scjr/rabbitconsumer/RabbitConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.scjr.droneconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
