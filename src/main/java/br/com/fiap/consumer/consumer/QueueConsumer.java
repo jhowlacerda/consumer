de37f14717553d0ef536259a084a0b5b8e139094
jhowlacerda <jonatha.lacerda@outlook.com>
2022-11-28 03:15:29 -0300

    Implementação Inicial envio de email

--- src/main/java/br/com/fiap/consumer/consumer/QueueConsumer.java
+++ src/main/java/br/com/fiap/consumer/consumer/QueueConsumer.java
@@ -1,23 +1,36 @@
 package br.com.fiap.consumer.consumer;
 
-import br.com.fiap.consumer.tarefasAgendadas.ValidaTemperaturaUmidade;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
 
 @Component
 public class QueueConsumer {
 
 
     public static final Logger log =LoggerFactory.getLogger(QueueConsumer.class);
+    ArrayList<String> dadosqueue = new ArrayList();
 
 
-    @RabbitListener(queues = "${queue.fiap}")
+   @RabbitListener(queues = "${queue.drone}")
     public void receiveMessage(@Payload String fileBody){
-        ValidaTemperaturaUmidade a = new ValidaTemperaturaUmidade();
+        dadosqueue.add(fileBody);
     }
+
+    @Scheduled(fixedRate = 4000)
+    public void Relatório(){
+
+        for (int i = 0; i<dadosqueue.size(); i++){
+        log.info("Dados do drone: " + dadosqueue.get(i));
+        }
+
+    }
+
 
     }
 
