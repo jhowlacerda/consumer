e1c596442c813c1ca63b885117d8e247cc016474
jhowlacerda <jonatha.lacerda@outlook.com>
2022-12-03 12:23:25 -0300

    Retirada do envio de email

--- src/main/java/br/com/fiap/consumer/consumer/QueueConsumer.java
+++ src/main/java/br/com/fiap/consumer/consumer/QueueConsumer.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 @Component
 public class QueueConsumer {
@@ -15,7 +16,9 @@ public class QueueConsumer {
 
     public static final Logger log =LoggerFactory.getLogger(QueueConsumer.class);
     ArrayList<String> dadosqueue = new ArrayList();
-
+    Random temperaturaaleatoria = new Random();
+    Random umidadealeatoria = new Random();
+    int id_drone = 1;
 
    @RabbitListener(queues = "${queue.drone}")
     public void receiveMessage(@Payload String fileBody){
@@ -24,9 +27,10 @@ public class QueueConsumer {
 
     @Scheduled(fixedRate = 4000)
     public void Relatório(){
-
+        float temperatura = temperaturaaleatoria.nextFloat()*50-20;
+        float umidade = umidadealeatoria.nextFloat()*40-20;
         for (int i = 0; i<dadosqueue.size(); i++){
-        log.info("Dados do drone: " + dadosqueue.get(i));
+        log.info("Dados do drone: " + dadosqueue.get(i) +  "Temperatura: "+temperatura + " Umidade: "+ umidade+" ID Drone: "+ id_drone);
         }
 
     }
